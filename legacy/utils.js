c06ddbd58869f06c70c1b86da680e98dfc3023ad
Matthew Connelly <matthew@harbourshare.com>
2025-06-02 16:21:52 -0400

    commit

---
+++ legacy/utils.js
@@ -0,0 +1,249 @@
+import { TextSelection, Selection } from 'prosemirror-state';
+import { Storage } from '@google-cloud/storage';
+import { JSDOM } from 'jsdom';
+import https from 'https';
+
+// In Node, we use the Editor class directly from superdoc/super-editor
+import { Editor, getStarterExtensions } from '@harbour-enterprises/superdoc/super-editor';
+
+const DOCX_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+
+// AI Utils
+const getDataFromStreamedResult = async (response) => {
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder();
+  let result = "";
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        console.log("Stream finished");
+        break;
+      }
+      const textChunk = decoder.decode(value);
+      result += textChunk;
+    }
+    return result;
+  } catch (error) {
+    console.error("Error reading stream:", error);
+  } finally {
+    reader.releaseLock();
+  }
+};
+
+const getJSONFromResult = (AIResult) => {
+  let jsonString = AIResult.split("```")[1];
+  jsonString = jsonString.replace("json\n{", "{");
+  return JSON.parse(jsonString);
+}
+
+const getAIResponse = async (editor) => {
+  const xml = editor.state.doc.textContent;
+  const prompt = `
+  Find the phrase after which a GDPR clause should be inserted, then find the phrase after: "${xml}"
+  Then, generate a GDPR clause. Do not include placeholders or templating.
+  Return your results in a JSON response like this:
+  {
+    clauseBefore,
+    clause,
+    clauseAfter
+  }
+  `;
+
+  const payload = {
+    stream: true,
+    context:
+      "You are an expert copywriter and you are immersed in a document editor. You are to provide document related text responses based on the user prompts. Only write what is asked for. Do not provide explanations. Try to keep placeholders as short as possible. Do not output your prompt. Your instructions are: ",
+    insights: [
+      {
+        type: "custom_prompt",
+        name: "text_generation",
+        message: prompt,
+        format: [{ value: "" }],
+      },
+    ],
+    document_content: xml,
+  };
+
+  const apiEndpoint = "https://sd-dev-express-gateway-i6xtm.ondigitalocean.app/insights";
+  const response = await fetch(apiEndpoint, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload),
+  });
+  
+  return response;
+}
+
+const getDataFromAIResponse = async ({AIResponse, editor}) => {
+  const result = await getDataFromStreamedResult(AIResponse);
+  const json = getJSONFromResult(result);
+  const {
+    clauseBefore,
+    clause,
+    clauseAfter,
+  } = json;
+  console.log("Phrase to insert after:", clauseBefore);
+
+  const position = getClausePosition(editor, clauseBefore);
+
+  return {
+    position,
+    clauseBefore,
+    clause,
+    clauseAfter,
+  };
+}
+
+const generateSignedUrl = async (bucketName, objectName, expirationTime, action) => {
+  const storage = new Storage();
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(objectName);
+
+  let options = {
+    version: 'v4',
+    action,
+    expires: expirationTime,
+  }
+
+  if (action === 'write') {
+    options = {
+      ...options,
+      contentType: DOCX_MIME_TYPE, // Or appropriate content type
+    }
+  }
+
+  const [signedUrl] = await file.getSignedUrl(options);
+
+  return signedUrl;
+}
+
+// cloud storage variables
+const bucketName = "harbour-prod-webapp.appspot.com/slackbot-uploads";
+const expirationTime = new Date(new Date().getTime() + 3600 * 1000); // 1 hour from now
+
+// upload to cloud store
+const uploadToSignedUrl = (uploadUrl, data) => {
+  const signedUrlPath = uploadUrl.split("/").pop();
+  const requestPath = `/${bucketName}/${signedUrlPath}`;
+  const options = {
+    hostname: "storage.googleapis.com",
+    path: requestPath,
+    method: "PUT",
+    headers: {
+      "Content-Type": DOCX_MIME_TYPE,
+    },
+  };
+
+  const signedUrlReq = https.request(options, function (res) {
+    res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+      console.log("BODY: " + chunk);
+    });
+  });
+
+  signedUrlReq.write(data);
+  signedUrlReq.end();
+};
+
+const generateUploadDownloadUrls = async (objectName) => {
+  const urls = {
+    upload: null,
+    download: null
+  };
+
+  const uploadSignedUrl = await generateSignedUrl(
+    bucketName,
+    objectName,
+    expirationTime,
+    "write"
+  );
+  urls.upload = uploadSignedUrl;
+
+  // get download signed url
+  const downloadSignedUrl = await generateSignedUrl(
+    bucketName,
+    objectName,
+    expirationTime,
+    "read"
+  );
+  urls.download = downloadSignedUrl;
+
+  return urls;
+}
+
+// Editor utils
+const getClausePosition = (editor, phrase) => {
+  const searchResult = editor.commands.search(phrase).pop();
+  if (!searchResult) {
+    console.log("Clause insert position not found");
+    return null;
+  }
+  const {from, to} = searchResult;
+  return to;
+}
+
+const positionCursor = (editor, toPos) => {
+  let selection = null;
+  if (!toPos) {
+    selection = Selection.atEnd(editor.view.docView.node)
+  } else {
+    selection = new TextSelection(editor.view.state.doc.resolve(toPos+1));
+  }
+  const tr = editor.view.state.tr.setSelection(selection);
+  const state = editor.view.state.apply(tr)
+  editor.view.updateState(state)
+};
+
+const getEditor = async (docxFileBuffer) => {
+  // For now, this is boilerplate code to mock the window and document
+  const { window: mockWindow } = (new JSDOM('<!DOCTYPE html><html><body></body></html>'));
+  const { document: mockDocument } = mockWindow;
+
+  // Prepare document data for the editor
+  const [content, mediaFiles] = await Editor.loadXmlData(docxFileBuffer);
+  // console.log(">>> CONTENT", content)
+
+  return new Editor({
+    user: {
+      name: "Superdoc",
+      // email: "matthew@harbourshare.com",
+      email: null,
+      image: null
+    },
+
+    isHeadless: true,
+
+    // We pass in the mock document and window here
+    mockDocument,
+    mockWindow,
+
+    // Our standard list of extensions
+    extensions: getStarterExtensions(),
+
+    // Our prepaerd document data
+    content,
+    mediaFiles,
+  });
+};
+
+const insertSuggestion = ({editor, position, clause}) => {
+  editor.setDocumentMode("suggesting");
+  editor.commands.enableTrackChanges();
+  positionCursor(editor, position);
+  editor.commands.insertContent(`<br /><br />${clause}<br /><br />`);
+};
+
+export {
+  getAIResponse,
+  generateUploadDownloadUrls,
+  uploadToSignedUrl,
+  positionCursor,
+  getEditor,
+  getDataFromAIResponse,
+  insertSuggestion,
+  DOCX_MIME_TYPE
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // import './file-polyfill.js'; Import the file polyfill if using NodeJS < v22.0.0
 import fs from 'fs';
-import {readFile} from 'fs/promises';
+import {readFile, writeFile} from 'fs/promises';
 import express from 'express';
 // import multer from 'multer';
+import https from 'https';
 import Busboy from 'busboy';
 import path from 'path'
 import { fileURLToPath } from 'url';
@@ -22,8 +23,8 @@ import {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Init your server of choice. For simplicity, we use express here
 const server = express();
+server.use(express.json());
 
 server.get('/', async (req, res, next) => {
   res
@@ -79,77 +80,45 @@ server.post('/text', async (req, res, next) => {
   req.pipe(busboy);
   // busboy.end(req.rawBody)
 })
+// https://zapier-dev-files.s3.amazonaws.com/cli-platform/20240/x-ZIRXjDbyxuHJi_5LcSc73SgbOeETN9oEqaTuDyLpbndL0_m-l7bV-vDAXH4auCgCE9fGJ6-gLB0fdF3pvcUwJKzDmd99Yd_yiIP4KPYjL0rDHVnHpjx5s2eUsHizeGtkQHKRX1-M0xgZ0qe5as4Qz3YqNJ17VV7RA6xEccXEQ
 
 server.post('/insert', async (req, res, next) => {
-  let filePath = null;
-  let uploadedFileName = null;
+  const json = req.body;
+  const {
+    clauseBefore,
+    clause,
+    clauseAfter,
+    file
+  } = json;
 
-  let busboy = null;
-  // missing headers?
-  try {
-    busboy = new Busboy({ headers: req.headers });
-  } catch (e) {
-    res.status(200).send({
-      success: false
-    });
-    return;
-  }
+  // const fileResponse = await fetch(file);
+  // console.log(">>> FR", fileResponse)
+  const fileName = generateRandomFilename();
+  const filePath = `./${fileName}`;
+  const fileStream = fs.createWriteStream(filePath);
+  await new Promise((resolve, reject) => {
+    https.get(file, (response) => {
+      response.pipe(fileStream);
 
-  const fields = {};
-  busboy.on('field', (fieldname, value) => {
-    fields[fieldname] = value;
-  });
-
-  // save file
-  busboy.on('file', async (fieldname, file, filename, encoding, mimetype) => {
-    uploadedFileName = filename || generateRandomFilename();
-    filePath = path.join(__dirname, uploadedFileName);
-    file.pipe(fs.createWriteStream(filePath));
-
-    console.log(">>> Uploaded file name:", uploadedFileName);
-  });
-
-  // after file save
-  busboy.on("finish", async () => {
-    // init signed urls
-    const {
-      upload: uploadUrl,
-      download: downloadUrl
-    } = await generateUploadDownloadUrls(uploadedFileName);
-
-    // init editor & modify document
-    let documentData = await readFile(filePath);
-    // TODO - handle atob error
-    let editor = null;
-    try {
-      editor = await getEditor(documentData);
-    } catch (e) {
-      res.status(200).send({
-        success: false
+      // after download completed close filestream
+      fileStream.on("finish", () => {
+        fileStream.close();
+        console.log("Download Completed");
+        resolve();
       });
-      return;
-    }
-    
-    // generate clause and find position
-    // const AIResponse = await getAIResponse(editor);
-    // const {
-    //   clauseBefore,
-    //   clause,
-    //   clauseAfter,
-    //   position
-    // } = await getDataFromAIResponse({AIResponse, editor});
+    });
+  });
 
-    const clause = fields['clause'];
-    const clauseBefore = fields['clause-before'];
-    const clauseAfter = fields['clause-after'];
+  let documentData = await readFile(filePath);
 
-    const position = getClausePosition(editor, clauseBefore);
+  let editor = null;
+  editor = await getEditor(documentData);
+  const position = getClausePosition(editor, clauseBefore);
 
-    // insert suggestion
-    insertSuggestion({editor, position, clause});
+  // insert suggestion
+  insertSuggestion({editor, position, clause});
 
-    // Export the docx and create a buffer to return to the user
-    let zipBuffer = null;
+      let zipBuffer = null;
     try {
       zipBuffer = await editor.exportDocx();
     } catch (e) {
@@ -158,23 +127,125 @@ server.post('/insert', async (req, res, next) => {
       });
       return;
     }
-    documentData = Buffer.from(zipBuffer);
 
+    const exportedData = Buffer.from(zipBuffer);
+    const {
+      upload: uploadUrl,
+      download: downloadUrl
+    } = await generateUploadDownloadUrls(fileName);
     // upload file
-    uploadToSignedUrl(uploadUrl, documentData);
+    uploadToSignedUrl(uploadUrl, exportedData);
 
-    // respond with download url
-    res.status(200).send({
-      success: true,
-      file: downloadUrl,
-      // clauseBefore,
-      // clause,
-      // clauseAfter,
-    });
+  res.status(200).send({
+    success: true,
+    file: downloadUrl
   });
 
-  req.pipe(busboy);
-  // busboy.end(req.rawBody)
-})
+  // await new Promise((resolve, reject) => {
+  //   request(file).pipe(fileStream);
+  //   fileStream.on("finish", resolve);
+  // });
+
+  // req.pipe(busboy);
+});
+  // const editor = await getEditor(documentData);
+
+
+// server.post('/insert', async (req, res, next) => {
+//   let filePath = null;
+//   let uploadedFileName = null;
+
+//   let busboy = null;
+//   // missing headers?
+//   try {
+//     busboy = new Busboy({ headers: req.headers });
+//   } catch (e) {
+//     res.status(200).send({
+//       success: false
+//     });
+//     return;
+//   }
+
+//   const fields = {};
+//   busboy.on('field', (fieldname, value) => {
+//     fields[fieldname] = value;
+//   });
+
+//   // save file
+//   busboy.on('file', async (fieldname, file, filename, encoding, mimetype) => {
+//     uploadedFileName = filename || generateRandomFilename();
+//     filePath = path.join(__dirname, uploadedFileName);
+//     file.pipe(fs.createWriteStream(filePath));
+
+//     console.log(">>> Uploaded file name:", uploadedFileName);
+//   });
+
+//   // after file save
+//   busboy.on("finish", async () => {
+//     // init signed urls
+//     const {
+//       upload: uploadUrl,
+//       download: downloadUrl
+//     } = await generateUploadDownloadUrls(uploadedFileName);
+
+//     // init editor & modify document
+//     let documentData = await readFile(filePath);
+//     // TODO - handle atob error
+//     let editor = null;
+//     try {
+//       editor = await getEditor(documentData);
+//     } catch (e) {
+//       res.status(200).send({
+//         success: false
+//       });
+//       return;
+//     }
+    
+//     // generate clause and find position
+//     // const AIResponse = await getAIResponse(editor);
+//     // const {
+//     //   clauseBefore,
+//     //   clause,
+//     //   clauseAfter,
+//     //   position
+//     // } = await getDataFromAIResponse({AIResponse, editor});
+
+//     const clause = fields['clause'];
+//     const clauseBefore = fields['clause-before'];
+//     const clauseAfter = fields['clause-after'];
+
+//     const position = getClausePosition(editor, clauseBefore);
+
+//     // insert suggestion
+//     insertSuggestion({editor, position, clause});
+
+//     // Export the docx and create a buffer to return to the user
+//     let zipBuffer = null;
+//     try {
+//       zipBuffer = await editor.exportDocx();
+//     } catch (e) {
+//       res.status(200).send({
+//         success: false
+//       });
+//       return;
+//     }
+//     documentData = Buffer.from(zipBuffer);
+
+//     // upload file
+//     uploadToSignedUrl(uploadUrl, documentData);
+
+//     // respond with download url
+//     res.status(200).send({
+//       success: true,
+//       file: downloadUrl,
+//       // clauseBefore,
+//       // clause,
+//       // clauseAfter,
+//     });
+//   });
+
+//   req.pipe(busboy);
+//   // busboy.end(req.rawBody)
+// })
 
 server.listen(8080, '0.0.0.0', () => console.debug(`Server running on port 8080`));
